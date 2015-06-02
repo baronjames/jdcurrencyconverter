6d3b7f852a7f03b61777cde2a7057278ac63bc00
James Duncan <james.paul.duncan1989@gmail.com>
2015-06-02 15:22:51 +0930

    MainActivity: Added Override Methods - onStart, onRestart, onResume onPause, onStop, onDestroy. Added "TAG" variable for LogCat. Added logging for override methods. Added logging for exception handling.

--- app/src/main/java/com/jamespaulduncan/converter/currency/jdcurrencyconverter/MainActivity.java
+++ app/src/main/java/com/jamespaulduncan/converter/currency/jdcurrencyconverter/MainActivity.java
@@ -1,25 +1,77 @@
 package com.jamespaulduncan.converter.currency.jdcurrencyconverter;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity
 {
+    //"TAG" variable used for debugging in LogCat
+    private static final String TAG = "JD CC: MainActivity";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        Log.d(TAG, "onCreate ~");
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        try
+        {
+            setContentView(R.layout.activity_main);
+
+
+        }
+        catch (Exception e)
+        {
+            Log.e(TAG, "onCreate ~ " + e.toString());
+        }
     }
 
+    @Override
+    protected void onStart()
+    {
+        Log.d(TAG, "onStart ~");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        Log.d(TAG, "onResume ~");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        Log.d(TAG, "onPause ~");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        Log.d(TAG, "onStop ~");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        Log.d(TAG, "onDestroy ~");
+        super.onDestroy();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
+        Log.d(TAG, "onCreateOptionsMenu ~");
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
@@ -28,6 +80,7 @@ public class MainActivity extends ActionBarActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
+        Log.d(TAG, "onOptionsItemSelected ~");
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
