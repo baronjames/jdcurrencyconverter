478b04771fb60ec17faa3569e93fe536576e0bfe
James Duncan <james.paul.duncan1989@gmail.com>
2015-06-03 18:27:54 +0930

    DB setup
    ConverterDB_RBA:
    
    Added SQLite helper class

--- app/src/main/java/com/jamespaulduncan/converter/currency/jdcurrencyconverter/ConverterDB_RBA.java
+++ app/src/main/java/com/jamespaulduncan/converter/currency/jdcurrencyconverter/ConverterDB_RBA.java
@@ -1,5 +1,10 @@
 package com.jamespaulduncan.converter.currency.jdcurrencyconverter;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
 /**
  * Created by James on 2/06/2015.
  * This DB will store the exchange rates as provided by the Reserve Bank of Australia
@@ -13,9 +18,11 @@ public class ConverterDB_RBA
     //"TAG" variable used for debugging in LogCat
     private static final String TAG = "JD CC: ConverterDB";
 
+    //DB Settings
     private static final String DATABASE_NAME = "currencyConverter_DB.db";
     private static final int DATABASE_VERSION = 1;
 
+    private Context local_context = null;
 
 
     //Define default currency 3 letter names
@@ -41,6 +48,7 @@ public class ConverterDB_RBA
             R.string.string_AED, R.string.string_CAD, R.string.string_PHP
     };
 
+    //Icon ID's: Integer
     public static final Integer[] currency_icon = {
             R.drawable.aud_flag, R.drawable.usd_flag, R.drawable.cny_flag,
             R.drawable.jpy_flag, R.drawable.eur_flag, R.drawable.krw_flag,
@@ -51,5 +59,111 @@ public class ConverterDB_RBA
             R.drawable.aed_flag, R.drawable.cad_flag, R.drawable.php_flag,
     };
 
+
+//    DB Table: CurrencyConverterRates
+//
+//    Columns:
+//
+//    currency_name     TEXT
+//    conversion_rate   FLOAT
+
+    private static final String TABLE_NAME = "CurrencyConverterRates";
+    private static final String COLUMN_NAME = "currency_name";
+    private static final String COLUMN_RATE = "conversion_rate";
+
+    //DB Helper Class
+    private class Converter_DB_Helper extends SQLiteOpenHelper
+    {
+        public Converter_DB_Helper(Context context)
+        {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+            //create backup
+            local_context = context;
+        }
+
+        /**
+         * Called when the database is created for the first time. This is where the
+         * creation of tables and the initial population of the tables should happen.
+         *
+         * @param db The database.
+         */
+        @Override
+        public void onCreate(SQLiteDatabase db)
+        {
+            try
+            {
+                String sql_string;
+
+                //create db if none exists
+                sql_string = "CREATE TABLE " + TABLE_NAME + " ( " +
+                        COLUMN_NAME + " TEXT"   + ", " +
+                        COLUMN_RATE + " FLOAT"  + " );";
+                Log.d(TAG, "Create Tables: SQL String = " + sql_string);
+
+                db.execSQL(sql_string);
+
+                //setup tables
+                for(int i=0; i<currency_name.length; i++)
+                {
+                    sql_string = "INSERT INTO " + TABLE_NAME + " ( " +
+                            COLUMN_NAME + " TEXT"   + ", " +
+                            COLUMN_RATE + " FLOAT"  + " ) " +
+                            "VALUES ('" +currency_name[i] + "', 1.0);";
+                    Log.d(TAG, "Populate Tables: SQL String = " + sql_string);
+                    //execute query
+                    db.execSQL(sql_string);
+                }
+                //update from file in raw folder
+                UpdateTableFromFile(db);
+            }
+            catch (Exception e)
+            {
+                Log.e(TAG, "onCreate:" + e.toString());
+            }
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+        {
+            Log.i(TAG, "Update DB:" +
+                    " old version = " + Integer.toString(oldVersion) +
+                    " new version = " + Integer.toString(newVersion));
+
+            //Delete old records and replace with new ones
+            try
+            {
+                String sql_string;
+
+                //delete all records
+                sql_string = "DELETE FROM " + TABLE_NAME + ";";
+                Log.d(TAG, "Delete all records: SQL String = " + sql_string);
+                db.execSQL(sql_string);
+
+                //setup tables
+                for(int i=0; i<currency_name.length; i++)
+                {
+                    sql_string = "INSERT INTO " + TABLE_NAME + " ( " +
+                            COLUMN_NAME + " TEXT"   + ", " +
+                            COLUMN_RATE + " FLOAT"  + " ) " +
+                            "VALUES ('" +currency_name[i] + "', 1.0);";
+                    Log.d(TAG, "Populate Tables: SQL String = " + sql_string);
+                    //execute query
+                    db.execSQL(sql_string);
+                }
+
+                //update from file in raw folder
+                UpdateTableFromFile(db);
+            }
+            catch (Exception e)
+            {
+                Log.e(TAG, "onUpdate:" + e.toString());
+            }
+        }
+
+        private void UpdateTableFromFile(SQLiteDatabase db)
+        {
+
+        }
+    }
 
 }
